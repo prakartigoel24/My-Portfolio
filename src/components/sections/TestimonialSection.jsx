c7d947d808ce80793c46ecff32893d2ee08341c1
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-11 18:26:54 +0530

    Add: Carousel testimonials using swiper

--- src/components/sections/TestimonialSection.jsx
+++ src/components/sections/TestimonialSection.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
-import '../../styles/testimonial.scss';
-import {testimonials} from '../../constants';
-import TestimonialItem from '../TestimonialItem';
+import React from "react";
+import "../../styles/testimonial.scss";
+import { testimonials } from "../../constants";
+import TestimonialItem from "../TestimonialItem";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation,Autoplay } from "swiper/modules";
 
 const TestimonialSection = () => {
   return (
     <section className="section section_primary section_testimonial">
       <h2>Testimonials</h2>
       <div className="container">
-        {testimonials.map((item, i)=>{
-          return (
-            <TestimonialItem 
-            key={i}
-            name={item.name}
-            designation={item.designation}
-            company={item.company}
-            image={item.image}
-            testimonial={item.testimonial}
-            />
-          )
-        })}
+        <Swiper
+          modules={[Autoplay, Navigation]}
+          spaceBetween={5}
+          slidesPerView={"auto"}
+          navigation
+          autoplay={{ delay: 1500,disableOnInteraction: false, }}
+          loop={true}
+        >
+          {testimonials.map((item, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <TestimonialItem
+                  name={item.name}
+                  designation={item.designation}
+                  company={item.company}
+                  image={item.image}
+                  testimonial={item.testimonial}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TestimonialSection
+export default TestimonialSection;
