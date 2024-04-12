458cccf199f327d946e049f389ad821328b92759
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-12 22:43:19 +0530

    Completed portfolio page for mobile devices

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import SubpageHero from '../SubpageHero';
+import PortfolioSection from '../sections/PortfolioSection';
+import ContactForm from '../ContactForm';
+import Footer from '../Footer';
 
 const PortfolioPage = () => {
   return (
-    <div>PortfolioPage</div>
+    <>
+      <main>
+        <SubpageHero
+          heading={"Projects Showcase"}
+          subheading={"Welcome aboard the dungeon of my projects in different modern technologies."}
+          backgroundImg={"bg-prism-spiral"}
+          btnText={"Contact Me"}
+          btnLink={"/contact"}
+        />
+      </main>
+      <div className='divider_accent'></div>
+      <PortfolioSection isSubsection={false}/>
+      <ContactForm/>
+      <Footer />
+    </>
   )
 }
 
