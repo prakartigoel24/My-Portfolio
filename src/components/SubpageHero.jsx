c2278dfed8c0ea2c0f536d8ead6ab311d9469cf9
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-12 21:06:46 +0530

    Added basic subpage hero section

--- src/components/SubpageHero.jsx
+++ src/components/SubpageHero.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import '../styles/subpageHero.scss';
+import { Link } from 'react-router-dom';
 
-const SubpageHero = () => {
+const SubpageHero = ({
+heading,
+subheading,
+backgroundImg,
+btnText,
+btnLink,
+}) => {
   return (
-    <div>SubpageHero</div>
+    <section className={`section section_subpage_hero ${backgroundImg}`}>
+      <div className="hero_1">
+        {/* <p>Hello, I am</p> */}
+        <h1>{heading}</h1>
+      </div>
+      <div className="hero_2">
+        <p>
+          {subheading}
+        </p>
+        <Link
+          to={btnLink}
+          className="btn_accent"
+        >
+          {btnText}
+        </Link>
+      </div>
+    </section>
   )
 }
 
--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SubpageHero from '../SubpageHero';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <main>
+      <SubpageHero
+      heading={"About Me"}
+      subheading={"Get to know more about me."}
+      backgroundImg={'bg-prism-spiral'}
+      btnText={'Contact Me'}
+      btnLink={'/contact'}/>
+    </main>
   )
 }
 
