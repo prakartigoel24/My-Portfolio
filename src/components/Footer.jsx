50af156d21bd0c208783d3ebf64ae147d9950996
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-12 18:54:55 +0530

    Footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../styles/footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="section section_primary footer">
+      <div className="social_info">
+        <h4>GET IN TOUCH</h4>
+        <a href="https://www.linkedin.com/in/prakarti-goel-2404/">
+          <img src="/icons/linkedin.png" alt="" />
+        </a>
+        <a href="mailto:prakartigoel24@outlook.com">
+          <img src="/icons/email.png" alt="" />
+        </a>
+        <a href="https://github.com/prakartigoel24">
+          <img src="/icons/github.png" alt="" />
+        </a>
+      </div>
+      <div className="divider"></div>
+      <p>&copy; Prakarti Goel. All rights reserved.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
