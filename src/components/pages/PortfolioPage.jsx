3dbaaaa921b6a1b9d4ad62d1065bc996fdd585ea
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-12 23:14:48 +0530

    Completed contact page for mobile devices

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -3,6 +3,7 @@ import SubpageHero from '../SubpageHero';
 import PortfolioSection from '../sections/PortfolioSection';
 import ContactForm from '../ContactForm';
 import Footer from '../Footer';
+import TestimonialSection from '../sections/TestimonialSection';
 
 const PortfolioPage = () => {
   return (
@@ -11,13 +12,15 @@ const PortfolioPage = () => {
         <SubpageHero
           heading={"Projects Showcase"}
           subheading={"Welcome aboard the dungeon of my projects in different modern technologies."}
-          backgroundImg={"bg-prism-spiral"}
+          backgroundImg={"bg-spiral"}
           btnText={"Contact Me"}
           btnLink={"/contact"}
         />
       </main>
       <div className='divider_accent'></div>
       <PortfolioSection isSubsection={false}/>
+      <div className='divider_secondary'></div>
+      <TestimonialSection/>
       <ContactForm/>
       <Footer />
     </>
