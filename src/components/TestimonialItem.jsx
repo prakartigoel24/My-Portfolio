d86462ffce5254561b8a885e9501f16b3bd3cd93
Prakarti Goel <prakartigoel24@gmail.com>
2024-04-14 00:10:46 +0530

    Fix: Testimonial layout for large screens

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -10,12 +10,11 @@ const TestimonialItem = ({
   return (
     <swiper-slide>
       <div className="testimonial_item">
-        <div className="testimonial_img">
+        <div className="testimonial_container">
           <img src={image} alt="Person's image" />
           <h4>{name}</h4>
           <h5>{`${designation} at ${company}`}</h5>
         </div>
-
         <p>{testimonial}</p>
       </div>
     </swiper-slide>
